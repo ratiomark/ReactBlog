43af645827d405b425324f20ce2a3b24819ab7d9
ratiomark <yanagae@gmail.com>
2023-02-20 18:58:17 +0200

    add RTK, add first entity, add storeProvider

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,9 @@
-import { Counter } from 'entities/Counter'
-import React from 'react'
 import { BugButton } from 'shared/ui/BugButton/BugButton'
 
 const MainPage = () => {
 	return (
 		<div>
 			MainPage
-			<Counter />
 			<BugButton />
 		</div>
 	)
