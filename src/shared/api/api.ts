d2c48672d9d986abd47a5dbfac107aae5d8246f2
ratiomark <yanagae@gmail.com>
2023-04-11 18:56:33 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -5,7 +5,17 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
 
 export const $api = axios.create({
 	baseURL: __API__,
-	headers: {
-		authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY)
+	// изначально заголовочная часть была тут, но перенес ее в интерцептор
+	// headers: {
+	// 	authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
+	// }
+})
+
+// создаю интерцептор, который будет отрабатыватся каждый раз перед тем как проходит запрос
+
+$api.interceptors.request.use((config) => {
+	if (config.headers) {
+		config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
 	}
+	return config
 })
